b447a592c9fa6f135d5be59dc660a3181e164419
yangchd <yangchd@yonyou.com>
2018-03-20 20:54:25 +0800

    LeetCode刷题记录

---
+++ src/main/java/com/yangchd/exercise/leetcode/easy/TwoSum.java
@@ -0,0 +1,26 @@
+package com.yangchd.exercise.leetcode.easy;
+
+/**
+ * @author yangchd  2018/3/20.
+ *
+ * Description:
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
+public class TwoSum {
+
+    /**
+     * 解答一：
+     * 这个方式速度较慢
+     */
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }
+}
