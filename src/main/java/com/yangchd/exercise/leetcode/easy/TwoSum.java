5c88de044eeadf302af1ee83030a56634a24d721
yangchd <yangchd@yonyou.com>
2018-03-20 20:58:51 +0800

    LeetCode刷题记录

--- src/main/java/com/yangchd/exercise/leetcode/easy/TwoSum.java
+++ src/main/java/com/yangchd/exercise/leetcode/easy/TwoSum.java
@@ -13,14 +13,16 @@ public class TwoSum {
      * 解答一：
      * 这个方式速度较慢
      */
-    public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            for(int i=0;i<nums.length;i++){
+                for(int j=i+1;j<nums.length;j++){
+                    if(nums[i]+nums[j]==target){
+                        return new int[]{i,j};
+                    }
                 }
             }
+            return null;
         }
-        return null;
     }
 }
